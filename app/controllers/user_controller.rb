a49f07d0247ad33a0cae22802216578a1917accf
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-03-09 02:48:29 -0500

    adds signup route in user_controller and create_user.erb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,20 @@
 class UserController < ApplicationController
     
+    #sign up
+    get '/signup' do
+        if !logged_in?
+            erb :'user/create_user', locals: {message: "Please sign up before you sign in"}
+          else
+            redirect to '/reviews'
+        end
+    end
+
+    #login
+    get '/login' do
+    end
+
+    #logout
+    get '/logout' do
+
+    end
 end
