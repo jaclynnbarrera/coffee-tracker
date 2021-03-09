e3c8988fb35e04db54e8935b417dae0e836d7940
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-03-09 02:32:38 -0500

    adds helper methods to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,16 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+    
+    def current_user
+      @user = User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+  end
+
 end
