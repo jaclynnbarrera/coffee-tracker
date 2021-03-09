a1702e765efb30b8e02b2aedbfc3abdde9e029f4
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-03-09 14:29:17 -0500

    updates post reviews route to include neighborhood params

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -10,7 +10,7 @@ class ReviewController < ApplicationController
 
     #processes form input and creates new review object
     post '/reviews' do 
-       @review = Review.create(:name=>params[:name], :rating=> params[:rating], :content => params[:content],:work_space => params[:work_space], :user_id => current_user.id)
+       @review = Review.create(:name=>params[:name], :neighborhood => params[:neighborhood], :rating=> params[:rating], :content => params[:content],:work_space => params[:work_space], :user_id => current_user.id)
        redirect to "/reviews/#{@review.id}"
     end
     
